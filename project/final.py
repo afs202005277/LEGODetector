1d29d95fd5bd7f7fbad2cd51205a9416d140ddb5
Andre Sousa <up202005277@up.pt>
2024-04-07 11:24:27 +0100

    added other approaches

--- project/final.py
+++ project/final.py
@@ -571,6 +571,354 @@ def process_images(input_file, output_file):
         json.dump(output_data, f, indent=2)
 
 
+#################################################################################
+#                        Beginning of other approaches tested                   #
+#################################################################################
+
+def display_images(images, window_names, dimensions=None):
+    named_images = zip(window_names, images)
+    for name, image in named_images:
+        resized_img = resize_image(image, dimensions)
+        cv2.imshow(name, resized_img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def remove_background_canny_v3(image_path):
+    image = cv2.imread(image_path)
+
+    # resize image
+    ratio = image.shape[1] / image.shape[0]
+    height = 800
+    width = int(height * ratio)
+
+    dim = (width, height)
+    image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image_hsv[:, :, 1] = np.clip(image_hsv[:, :, 1] + 12, 0, 255)
+    image_hsv[:, :, 2] = np.clip(image_hsv[:, :, 2] + 3, 0, 255)
+    image = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+
+    image = cv2.medianBlur(image, 15)
+    image = cv2.GaussianBlur(image, (3, 3), sigmaX=0)
+
+    edges = cv2.Canny(image, 50, 125)
+
+    edges = cv2.dilate(edges, None, iterations=10)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    result = np.zeros_like(image)
+    for contour in contours:
+        if cv2.contourArea(contour) > 0:
+            cv2.drawContours(result, [contour], 0, (255, 255, 255), cv2.FILLED)
+
+    result = cv2.bitwise_and(image, result)
+
+    return image, result, contours
+
+
+def remove_background_canny_v2(image_path):
+    image = cv2.imread(image_path)
+
+    # resize image
+    ratio = image.shape[1] / image.shape[0]
+    height = 800
+    width = int(height * ratio)
+
+    dim = (width, height)
+    image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image_hsv[:, :, 1] = np.clip(image_hsv[:, :, 1] + 12, 0, 255)
+    image_hsv[:, :, 2] = np.clip(image_hsv[:, :, 2] + 3, 0, 255)
+    image = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+
+    image = cv2.medianBlur(image, 15)
+    image = cv2.GaussianBlur(image, (3, 3), sigmaX=0)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 125)
+
+    edges = cv2.dilate(edges, None, iterations=10)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    result = np.zeros_like(image)
+    for contour in contours:
+        if cv2.contourArea(contour) > 0:
+            cv2.drawContours(result, [contour], 0, (255, 255, 255), cv2.FILLED)
+
+    result = cv2.bitwise_and(image, result)
+
+    return image, result
+
+
+def remove_background_canny(image_path):
+    image = cv2.imread(image_path)
+
+    # resize image
+    ratio = image.shape[1] / image.shape[0]
+    height, width = 0, 0
+    if ratio > 1:
+        width = 800
+        height = int(width / ratio)
+    else:
+        height = 800
+        width = int(height * ratio)
+
+    dim = (width, height)
+    image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    image = cv2.medianBlur(image, 15)
+    image = cv2.GaussianBlur(image, (5, 5), sigmaX=0)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 100, 150)
+
+    edges = cv2.dilate(edges, None, iterations=10)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    result = np.zeros_like(image)
+    for contour in contours:
+        if cv2.contourArea(contour) > 0:
+            cv2.drawContours(result, [contour], 0, (255, 255, 255), cv2.FILLED)
+
+    result = cv2.bitwise_and(image, result)
+
+    return result
+
+
+def get_blob_params():
+    params = cv2.SimpleBlobDetector_Params()
+
+    params.minThreshold = 5
+    params.maxThreshold = 220
+
+    params.filterByArea = True
+    params.minArea = (
+        750
+    )
+    params.maxArea = 10000
+
+    params.filterByCircularity = False
+    params.filterByConvexity = False
+    params.filterByInertia = False
+    params.filterByColor = True
+    params.blobColor = 0
+    return params
+
+
+def remove_background(image_path, hue_margin=30, sat_margin=100, val_margin=255):
+    image = cv2.imread(image_path)
+    original = image.copy()
+    image = cv2.GaussianBlur(image, (55, 55), sigmaX=0)
+
+    if image is None:
+        print("Error: Unable to read image.")
+        return
+
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    flattened_image = hsv_image.reshape((-1, 3))
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    _, labels, centers = cv2.kmeans(
+        np.float32(flattened_image), 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS
+    )
+    most_common_color = np.uint8(centers[0])
+
+    lower_bound = np.array(
+        [
+            most_common_color[0] - hue_margin,
+            most_common_color[1] - sat_margin,
+            most_common_color[2] - val_margin,
+        ]
+    )
+    upper_bound = np.array(
+        [
+            most_common_color[0] + hue_margin,
+            most_common_color[1] + sat_margin,
+            most_common_color[2] + val_margin,
+        ]
+    )
+
+    mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
+
+    background_mask = cv2.bitwise_not(mask)
+
+    result = cv2.bitwise_and(original, original, mask=background_mask)
+
+    display_images([original, result], ["Original Image", "Background Removed"], (800, 600))
+
+    return result
+
+
+def show_hue(image):
+    image = image.copy()
+
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    hue_channel = hsv_image[:, :, 0]
+
+    cv2.imshow("Hue Channel", cv2.medianBlur(hue_channel, 5))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def image_setup(image_name):
+    original = cv2.imread(image_name)
+    original = cv2.resize(original, (1133, 944))
+    # show_hue(original)
+    original = cv2.bilateralFilter(original, 11, 75, 75)
+    # original = cv2.GaussianBlur(original, (5, 5), 0)
+
+    gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    sharpening_kernel = np.array([[0, -1, 0], [-1, 9, -1], [0, -1, 0]])
+    sharpening_kernel = sharpening_kernel / np.sum(sharpening_kernel)
+    gray = cv2.filter2D(gray, -1, sharpening_kernel)
+    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(8, 8))
+    gray = clahe.apply(gray)
+
+    return original, gray
+
+
+def blob_detection(gray_image):
+    params = get_blob_params()
+    if cv2.__version__.startswith("2."):
+        detector = cv2.SimpleBlobDetector(params)
+    else:
+        detector = cv2.SimpleBlobDetector_create(params)
+
+    keypoints = detector.detect(gray_image)
+
+    img_key_points = cv2.drawKeypoints(
+        gray_image,
+        keypoints,
+        np.array([]),
+        (0, 0, 255),
+        cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+    )
+
+    display_images([img_key_points], ['Keypoints'])
+    return len(keypoints)
+
+
+def color_detection(image):
+    image = cv2.bilateralFilter(image, 9, 75, 75)
+
+    # Convert BGR image to HSV color space
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Define thresholds for each Lego color in HSV space
+    color_thresholds = {
+        "red": ([0, 100, 100], [10, 255, 255]),
+        "orange": ([11, 100, 100], [20, 255, 255]),
+        "yellow": ([21, 100, 100], [30, 255, 255]),
+        "green": ([31, 100, 100], [70, 255, 255]),
+        "blue": ([90, 100, 100], [130, 255, 255]),
+        "purple": ([131, 100, 100], [160, 255, 255]),
+        "white": ([0, 0, 200], [180, 50, 255]),
+        "black": ([0, 0, 0], [180, 255, 100]),
+    }
+
+    distinct_colors = set()
+
+    for color_name, (lower, upper) in color_thresholds.items():
+        mask = cv2.inRange(hsv_image, np.array(lower), np.array(upper))
+
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+
+        if contours:
+            distinct_colors.add(color_name)
+
+    display_images([image], ["Image with Contours"])
+
+    return len(distinct_colors)
+
+
+def make_image_brighter(image, factor):
+    image = image.copy()
+
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    h, s, v = cv2.split(hsv_image)
+
+    v_multiplied = np.clip(v * factor, 0, 255).astype(np.uint8)
+
+    hsv_image_modified = cv2.merge([h, s, v_multiplied])
+
+    modified_image = cv2.cvtColor(hsv_image_modified, cv2.COLOR_HSV2BGR)
+
+    cv2.imshow("Original Image", image)
+    cv2.imshow("Modified Image", modified_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return modified_image
+
+
+def _detect_pieces_v1(filename):
+    _, gray = image_setup(filename)
+    return blob_detection(gray)
+
+
+def _detect_pieces_v2(filename):
+    without_background = remove_background(filename)
+    gray_wo_bgr = cv2.cvtColor(without_background, cv2.COLOR_BGR2GRAY)
+    return blob_detection(cv2.equalizeHist(gray_wo_bgr))
+
+
+def _detect_pieces_v3(filename):
+    without_background = remove_background_canny(filename)
+    return len(db_scan(without_background))
+
+
+def _detect_pieces_v4(filename):
+    image, without_background = remove_background_canny_v2(filename)
+    clusters = db_scan(without_background)
+    pieces, colors = color_scan(clusters, without_background)
+    return pieces
+
+
+def _detect_pieces_v5(filename):
+    image, without_background, contours = remove_background_canny_v3(filename)
+    without_background, _ = image_segmentation(without_background, contours, image)
+    clusters = db_scan(without_background)
+    pieces, colors = color_scan(clusters, without_background)
+    return pieces
+
+
+def _count_colors_v1(filename):
+    original, _ = image_setup(filename)
+    return color_detection(original)
+
+
+def _count_colors_v2(filename):
+    without_background = remove_background(filename)
+    return color_detection(without_background)
+
+
+def _count_colors_v3(filename):
+    image, without_background = remove_background_canny_v2(filename)
+    clusters = db_scan(without_background)
+    pieces, colors = color_scan(clusters, without_background)
+    return colors
+
+
+def _count_colors_v4(filename):
+    image, without_background, contours = remove_background_canny_v3(filename)
+    without_background, _ = image_segmentation(without_background, contours, image)
+    clusters = db_scan(without_background)
+    pieces, colors = color_scan(clusters, without_background)
+    return colors
+
+
+#################################################################################
+#                        End of other approaches tested                         #
+#################################################################################
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python script.py input_file output_file")
