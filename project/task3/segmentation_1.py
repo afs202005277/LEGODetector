5f8e4e1a5eaa86964e4858da9bf1a31c15fcdb73
Goncalo Pinto <goncalopinto2002@gmail.com>
2024-06-05 19:58:59 +0100

    Updated pretrained

--- project/task3/segmentation_1.py
+++ project/task3/segmentation_1.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 from ultralytics.data.annotator import auto_annotate
 import random
 
-model = YOLO('yolov8n-seg.pt', task='segmentation')
+model = YOLO('yolov8l-seg.pt', task='segmentation')
 
 
 '''auto_annotate(data='datasets/vc/images/test', det_model='best_small.pt')
@@ -12,4 +12,4 @@ auto_annotate(data='datasets/vc/images/train', det_model='best_small.pt')
 auto_annotate(data='datasets/vc/images/val', det_model='best_small.pt')'''
 
 
-model.train(data='datasets/seg/data.yaml', epochs=50, batch=-1, model="best_nano.pt", dropout=0.1)
+model.train(data='datasets/seg/data.yaml', epochs=100, batch=-1, model="best_large_check.pt", dropout=0.1)
