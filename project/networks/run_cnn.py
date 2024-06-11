99c8e27af336fc58a87ee80fc9d3aab801251b9e
Daniel Rodrigues <up202006562@up.pt>
2024-06-11 23:17:47 +0100

    .

--- project/networks/run_cnn.py
+++ project/networks/run_cnn.py
@@ -4,8 +4,7 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from PIL import Image
 from tqdm import tqdm
-from sklearn.metrics import mean_absolute_error
-import os
+import json
 import random
 import argparse
 import torchvision.models as models
@@ -36,9 +35,8 @@ class CustomCNN(nn.Module):
 
 
 class LegoDataset(Dataset):
-    def __init__(self, image_paths, labels, transform=None):
+    def __init__(self, image_paths, transform=None):
         self.image_paths = image_paths
-        self.labels = labels
         self.transform = transform
 
     def __len__(self):
@@ -47,12 +45,11 @@ class LegoDataset(Dataset):
     def __getitem__(self, idx):
         img_path = self.image_paths[idx]
         image = Image.open(img_path).convert("RGB")
-        label = self.labels[idx]
 
         if self.transform:
             image = self.transform(image)
 
-        return image, label
+        return image
 
 
 def parse_args():
@@ -60,8 +57,10 @@ def parse_args():
     parser.add_argument('--model_name', type=str, default='custom',
                         choices=['custom', 'resnet', 'vgg', 'densenet', 'efficientnet'])
     parser.add_argument('--path_to_model', type=str, default='models/custom.pth')
-    parser.add_argument('--img_folder', type=str, default='imgs')
     parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--input_json', type=str, default='test.json')
+    parser.add_argument('--output_json', type=str, default='output.json')
+    return parser.parse_args()
 
 
 def get_tarnsformer(model_name):
@@ -117,84 +116,52 @@ def get_model(model_name, path_to_model):
     return model
 
 
-def epoch_iter(dataloader, model, loss_fn, device, model_name):
-    if model_name == 'custom':
-        need_unsqueeze = False
-    else:
-        need_unsqueeze = True
-
+def epoch_iter(dataloader, model, device, model_name):
     # Get number of batches
-    num_batches = len(dataloader)
-
     model.eval()
 
-    # Define variables to save predictions and labels during the epoch
-    total_loss = 0.0
     preds = []
-    labels = []
 
     # Enable/disable gradients based on whether the model is in train or evaluation mode
     with torch.set_grad_enabled(False):
 
         # Analyse all batches
-        for batch, (X, y) in enumerate(tqdm(dataloader)):
-
-            # Put data in same device as model (GPU or CPU)
-            X, y = X.to(device), y.to(device)
+        for batch, X in enumerate(tqdm(dataloader)):
+            X = X.to(device)
 
             # Forward pass to obtain prediction of the model
             pred = model(X)
 
-            # Compute loss between prediction and ground-truth
-            if need_unsqueeze:
-                loss = loss_fn(pred, y.float().unsqueeze(1))
-            else:
-                loss = loss_fn(pred, y.float())
-
-            # Save training metrics
-            total_loss += loss.item()  # IMPORTANT: call .item() to obtain the value of the loss WITHOUT the computational graph attached
-
-            # Add predictions
             preds.extend(pred.detach().cpu().numpy())
-            labels.extend(y.cpu().numpy())
 
-    return total_loss / num_batches, mean_absolute_error(labels, preds), labels, preds
+    if model_name == 'custom':
+        preds_int = list(map(lambda x: max(int(x + 0.5), 1), preds))
+    else:
+        preds_int = list(map(lambda x: max(int(x[0] + 0.5), 1), preds))
 
-
-def list_files(folder_path):
-    queue = [folder_path]
-    files = []
-    while queue:
-        current_path = queue.pop(0)
-        if os.path.isdir(current_path):
-            queue.extend([os.path.join(current_path, filename) for filename in os.listdir(current_path)])
-        else:
-            files.append(current_path)
-
-    return files
+    return preds_int
 
 
-def get_labels(img_paths):
-    return list(map(lambda x: int(x[x.rfind('_') + 1:x.rfind('.')]), img_paths))
+def get_json_file(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
 
 
 def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    path_imgs = list_files(args.img_folder)
-    labels = get_labels(path_imgs)
-    dataset = LegoDataset(path_imgs, labels, transform=get_tarnsformer(args.model_name))
+    path_imgs = get_json_file(args.img_folder)
+    dataset = LegoDataset(path_imgs, transform=get_tarnsformer(args.model_name))
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
 
     model = get_model(args.model_name, args.path_to_model)
     model.to(device)
 
-    loss_fn = nn.MSELoss()
+    preds = epoch_iter(dataloader, model, device, args.model_name)
 
-    loss, mae, labels, preds = epoch_iter(dataloader, model, loss_fn, device, args.model_name)
-
-    print(f'Loss: {loss}')
-    print(f'MAE: {mae}')
+    with open(args.output_json, 'w') as file:
+        json.dump(preds, file)
 
 
 if __name__ == '__main__':
